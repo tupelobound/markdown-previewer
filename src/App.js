287773343a52c13d7fd523523ce5d157dfece912
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-04 20:17:02 +0000

    Added logic for expanding and shrinking boxes

--- src/App.js
+++ src/App.js
@@ -47,6 +47,11 @@ This project was created using React. Here's the lovely React logo:
 function App() {
   // set initial state of output box
   const [output, setOutput] = useState(placeholder)
+  // set initial state of editor and preview boxes, expanded or not
+  const [expanded, setExpanded] = useState({
+    editor: false,
+    preview: false
+  })
 
   // Function to handle change in the text area and pass this data to state
   const handleChange = event => {
@@ -55,7 +60,53 @@ function App() {
 
   // Function to handle click events for expanders
   const handleClick = event => {
-    console.log(event)
+    // If the editor expand button is pressed:
+    if (event.target.id === "editor-expand") {
+      // And if the the editor isn't already expanded
+      if (!expanded.editor) {
+        document.getElementById("preview-container").className = "invisible"; // Make the preview box invisible
+        document.getElementById("editor-container").className = "vb-container-full"; // Expand the editor box
+        document.getElementById("editor-expand").className = "fas fa-compress-arrows-alt fa-lg" // Change the icon
+        // Set the new state
+        setExpanded({
+          ...expanded,
+          editor: true
+        })
+        // Otherwise:
+      } else {
+        document.getElementById("preview-container").className = "view-box-container"; // Make the preview box visible
+        document.getElementById("editor-container").className = "view-box-container"; // Shrink the editor box
+        document.getElementById("editor-expand").className = "fas fa-expand-arrows-alt fa-lg" // Change the icon
+        // Set the new state
+        setExpanded({
+          ...expanded,
+          editor: false
+        })
+      }
+    } else {
+      // If the preview expand button is pressed
+      // And if the the preview isn't already expanded
+      if (!expanded.preview) {
+        document.getElementById("editor-container").className = "invisible"; // Make the editor box invisible
+        document.getElementById("preview-container").className = "vb-container-full"; // Expand the preview box
+        document.getElementById("preview-expand").className = "fas fa-compress-arrows-alt fa-lg" // Change the icon
+        // Set the new state
+        setExpanded({
+          ...expanded,
+          preview: true
+        })
+        // Otherwise:
+      } else {
+        document.getElementById("editor-container").className = "view-box-container"; // Make the editor box visible
+        document.getElementById("preview-container").className = "view-box-container"; // Shrink the preview box
+        document.getElementById("preview-expand").className = "fas fa-expand-arrows-alt fa-lg" // Change the icon
+        // Set the new state
+        setExpanded({
+          ...expanded,
+          preview: false
+        })
+      }
+    }
   }
 
   // Return the app content
