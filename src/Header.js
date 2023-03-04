b0b4beb713e35cae2b56a783cf510bf881a8452d
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-04 18:57:35 +0000

    Added Header component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1 id="header">Welcome to my Markdown Previewer</h1>
+      <p id="intro">Type text in the editor window and watch it change in the preview window!</p>
+    </div>
+  );
+}
+
+export default Header;
