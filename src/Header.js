a6b6a940caf71f76b5de778a3d0fb21f87f844f8
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-09-24 21:10:43 +0100

    Updates header text

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header () {
   return (
     <div>
       <h1 id="header">Welcome to my Markdown Previewer</h1>
-      <p id="intro">Type text in the editor window and watch it change in the preview window!</p>
+      <p id="intro">Type Markdown in the editor window and watch it change in the preview window!</p>
     </div>
   )
 }
